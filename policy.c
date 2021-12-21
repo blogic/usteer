918b809d6465a18444f7796ead1251c5a37dc6f9
David Bauer <mail@david-bauer.net>
2021-12-21 13:24:14 +0100

    policy: only select candidates with better load
    When kicking clients due to high channel load, explicitly only select
    new candidates in case their channel load is an improvement over the
    current node.
    
    Signed-off-by: David Bauer <mail@david-bauer.net>

--- policy.c
+++ policy.c
@@ -454,7 +454,7 @@ usteer_local_node_kick(struct usteer_local_node *ln)
 		if (is_more_kickable(kick1, si))
 			kick1 = si;
 
-		tmp = find_better_candidate(si, NULL, 0);
+		tmp = find_better_candidate(si, NULL, (1 << UEV_SELECT_REASON_LOAD));
 		if (!tmp)
 			continue;
 
